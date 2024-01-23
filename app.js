5585a03f7c29902b90eae9851367e9eb6240f9b8
rewaa1 <rewaayasser@hotmail.com>
2024-01-23 21:17:35 +0200

    last edit

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 
 // Middleware to check working hours
-// const workingHoursMiddleware = (req, res, next) => {
-//   const now = new Date();
-//   const dayOfWeek = now.getDay(); // Sunday is 0, Monday is 1, ..., Saturday is 6
-//   const hourOfDay = now.getHours();
+const workingHoursMiddleware = (req, res, next) => {
+  const now = new Date();
+  const dayOfWeek = now.getDay(); // Sunday is 0, Monday is 1, ..., Saturday is 6
+  const hourOfDay = now.getHours();
 
-//   if (dayOfWeek >= 1 && dayOfWeek <= 5 && hourOfDay >= 9 && hourOfDay < 17) {
-//     next();
-//   } else {
-//     res.status(503).send('The web application is only available during working hours.');
-//   }
-// };
+  if (dayOfWeek >= 1 && dayOfWeek <= 5 && hourOfDay >= 9 && hourOfDay < 17) {
+    next();
+  } else {
+    res.status(503).send('The web application is only available during working hours.');
+  }
+};
 
 // Use tailwindcss for styling
 app.use(express.static('public')); // Assuming your CSS is in a 'public' folder
 
 // Use custom middleware for all routes
-//app.use(workingHoursMiddleware);
+app.use(workingHoursMiddleware);
 
 // Define routes
 app.get('/', (req, res) => {
